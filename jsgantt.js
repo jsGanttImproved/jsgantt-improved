57f9453f8fa54caab00fb10ef7efc3a6fa65558e
jsGanttImproved <jsgantt.improved@gmail.com>
2016-05-01 17:47:03 +0100

    Don't try to hide a tool tip if it does not exist.

--- jsgantt.js
+++ jsgantt.js
@@ -1,16 +1,16 @@
 /*
-	   _   ___  _____   _  _____ ____
-	  (_) / _ \ \_   \ / ||___  | ___|
-	  | |/ /_\/  / /\/ | |   / /|___ \
-	  | / /_\\/\/ /_   | |_ / /_ ___) |
-	 _/ \____/\____/   |_(_)_/(_)____/
+	   _   ___  _____   _  _____ ____   _ 
+	  (_) / _ \ \_   \ / ||___  | ___| / |
+	  | |/ /_\/  / /\/ | |   / /|___ \ | |
+	  | / /_\\/\/ /_   | |_ / /_ ___) || |
+	 _/ \____/\____/   |_(_)_/(_)____(_)_|
 	|__/
-	jsGanttImproved 1.7.5
-	Copyright (c) 2013-2015, Paul Geldart All rights reserved.
+	jsGanttImproved 1.7.5.1
+	Copyright (c) 2013-2016, Paul Geldart All rights reserved.
 
 	The current version of this code can be found at https://code.google.com/p/jsgantt-improved/
 
-	* Copyright (c) 2013-2015, Paul Geldart.
+	* Copyright (c) 2013-2016, Paul Geldart.
 	* All rights reserved.
 	*
 	* Redistribution and use in source and binary forms, with or without
@@ -1540,7 +1540,7 @@ JSGantt.stripUnwanted=function(pNode){
 
 JSGantt.delayedHide=function(pGanttChartObj, pTool, pTimer){
 	var vDelay=1500;
-	pTool.delayTimeout=setTimeout(function(){JSGantt.hideToolTip(pGanttChartObj, pTool, pTimer);}, vDelay);
+	if(pTool) pTool.delayTimeout=setTimeout(function(){JSGantt.hideToolTip(pGanttChartObj, pTool, pTimer);}, vDelay);
 };
 
 JSGantt.hideToolTip=function(pGanttChartObj, pTool, pTimer){
