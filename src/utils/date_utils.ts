a16ed6ccc329dc00e9d0cc7b6fa135c813b443df
Carsten Schumann <carsten@familie-schumann.info>
2020-10-01 11:57:45 +0200

    Bugfix: Wrong start/end calculation when using planned dates in tasks.
    When using the task.planStart and task.planEnd fields the beginning/end of the gantt chart was caclulated incorrectly. Both getMinDate and getMaxDate now take planned and real dates into consideration.

--- src/utils/date_utils.ts
+++ src/utils/date_utils.ts
@@ -12,6 +12,7 @@ export const getMinDate = function (pList, pFormat, pMinDate) {
   // Parse all Task Start dates to find min
   for (let i = 0; i < pList.length; i++) {
     if (pList[i].getStart().getTime() < vDate.getTime()) vDate.setTime(pList[i].getStart().getTime());
+    if (pList[i].getPlanStart().getTime() < vDate.getTime()) vDate.setTime(pList[i].getPlanStart().getTime());
   }
 
   // Adjust min date to specific format boundaries (first of week or first of month)
@@ -58,6 +59,7 @@ export const getMaxDate = function (pList, pFormat, pMaxDate) {
   // Parse all Task End dates to find max
   for (let i = 0; i < pList.length; i++) {
     if (pList[i].getEnd().getTime() > vDate.getTime()) vDate.setTime(pList[i].getEnd().getTime());
+    if (pList[i].getPlanEnd().getTime() > vDate.getTime()) vDate.setTime(pList[i].getPlanEnd().getTime());
   }
 
   // Adjust max date to specific format boundaries (end of week or end of month)
